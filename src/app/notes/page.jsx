310dc615af95ed49cc644ef4c9598a72189c8be7
Dru-429 <ect.dhruv@gmail.com>
2025-06-15 23:58:11 +0530

    Adding Notes Page

---
+++ src/app/notes/page.jsx
@@ -0,0 +1,23 @@
+"use client"
+
+import Controls from '@/components/notes/Controls'
+import NoteCard from '@/components/notes/NoteCard'
+import { NoteContext } from '@/context/NoteContext'
+import React, { useContext } from 'react'
+
+const Notespage = () => {
+
+    const { notes } = useContext(NoteContext)
+    
+    return (
+        <div>
+            {notes.map((note) => (
+                <NoteCard note={note} key={note.$id} />
+            ))}
+
+            <Controls />
+        </div>
+    )
+}
+
+export default Notespage
